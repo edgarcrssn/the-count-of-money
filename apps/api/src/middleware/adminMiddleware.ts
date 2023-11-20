5a30865c010139406f70e8f32cc3a5dc643d9664
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-11-20 14:32:05 +0100

    fix: :recycle: fixed adminMiddleware response

--- apps/api/src/middleware/adminMiddleware.ts
+++ apps/api/src/middleware/adminMiddleware.ts
@@ -1,10 +1,15 @@
-import { Role } from "@prisma/client";
-import { authMiddleware } from "./authMiddleware";
+import { Role } from '@prisma/client';
+import { authMiddleware } from './authMiddleware';
 import { NextFunction, Request, Response } from 'express';
 
-export const adminMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const adminMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   authMiddleware(req, res, () => {
-    if (req.user.role !== Role.ADMIN) return res.sendStatus(403);
+    if (req.user.role !== Role.ADMIN)
+      return res.status(403).send({ message: 'Forbidden' });
     next();
-  })
+  });
 };
