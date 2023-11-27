a43a463f4f94b9755d7e58e9c4e54ea75591c846
GVercaemer <geoffrey.vercaemer@gmail.com>
2023-11-27 09:44:54 +0100

    Add controller functions to get all cryptos and one
    T-COM 12 T-COM 13

--- apps/api/src/routes/cryptos/cryptos.controller.ts
+++ apps/api/src/routes/cryptos/cryptos.controller.ts
@@ -1,11 +1,39 @@
 import { Request, Response } from 'express'
+import axios from 'axios'
 
-export const getCryptosController = (req: Request, res: Response) => {
-  res.send('getCryptosController')
+export const getCryptosController = async (req: Request, res: Response) => {
+  try {
+    const response = await axios.get(`${process.env.COINGECKO_API_URL}/coins/markets`, {
+      headers: {
+        Accept: 'application/json',
+        'Accept-Encoding': 'deflate, gzip',
+        x_cg_demo_api_key: process.env.COINGECKO_API_KEY,
+      },
+      // TODO: Make vs_currency dynamic based on user preferences
+      params: {
+        vs_currency: 'eur',
+      },
+    })
+    res.send(response.data)
+  } catch (error) {
+    res.status(500).send({ message: 'Error while fetching data from CoinGecko API: ', error })
+  }
 }
 
-export const getCryptoByIdController = (req: Request, res: Response) => {
-  res.send('getCryptoByIdController')
+export const getCryptoByIdController = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id
+    const response = await axios.get(`${process.env.COINGECKO_API_URL}/coins/${id}`, {
+      headers: {
+        Accept: 'application/json',
+        'Accept-Encoding': 'deflate, gzip',
+        x_cg_demo_api_key: process.env.COINGECKO_API_KEY,
+      },
+    })
+    res.send(response.data)
+  } catch (error) {
+    res.status(500).send({ message: 'Error while fetching data from CoinGecko API: ', error })
+  }
 }
 
 export const getCryptoPriceHistoryController = (req: Request, res: Response) => {
