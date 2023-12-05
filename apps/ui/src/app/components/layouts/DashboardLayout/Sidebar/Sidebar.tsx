aa07c054c6b9f32547e3e1226330684190f7706e
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-12-05 23:39:17 +0100

    feat(ui): :sparkles: refined sidebar links

--- apps/ui/src/app/components/layouts/DashboardLayout/Sidebar/Sidebar.tsx
+++ apps/ui/src/app/components/layouts/DashboardLayout/Sidebar/Sidebar.tsx
@@ -1,35 +1,68 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import styles from './Sidebar.module.scss'
-import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
+import { AreaChartOutlined, FileTextOutlined, HomeOutlined, UserOutlined } from '@ant-design/icons'
 import type { MenuProps } from 'antd'
 import { Layout, Menu } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { CurrentUserContext } from '../../../../context/CurrentUserContext'
 const { Sider } = Layout
 
 type MenuItem = Required<MenuProps>['items'][number]
 
-function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem
-}
-
-const items: MenuItem[] = [
-  getItem('Option 1', '1', <PieChartOutlined />),
-  getItem('Option 2', '2', <DesktopOutlined />),
-  getItem('User', 'sub1', <UserOutlined />, [getItem('Tom', '3'), getItem('Bill', '4'), getItem('Alex', '5')]),
-  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-  getItem('Files', '9', <FileOutlined />),
-]
-
 export const Sidebar = () => {
+  const { pathname } = useLocation()
+
   const [collapsed, setCollapsed] = useState(false)
+  const [selectedKeys, setSelectedKeys] = useState<string[]>([])
+
+  const { currentUser } = useContext(CurrentUserContext)
+
+  const menuItems: MenuItem[] = [
+    {
+      key: '1',
+      icon: <HomeOutlined />,
+      label: <Link to="/">Home</Link>,
+    },
+    currentUser
+      ? {
+          key: '2',
+          icon: <UserOutlined />,
+          label: <Link to={`/profile/${currentUser.nickname}`}>My profile</Link>,
+        }
+      : null,
+    {
+      key: '3',
+      icon: <AreaChartOutlined />,
+      label: <Link to="/cryptocurrencies">Cryptocurrencies</Link>,
+    },
+    {
+      key: '4',
+      icon: <FileTextOutlined />,
+      label: <Link to="/articles">Articles</Link>,
+    },
+  ]
+
+  useEffect(() => {
+    const getSelectedKeys = (): string[] => {
+      if (pathname === '/') return ['1']
+      if (pathname.startsWith('/profile')) return ['2']
+      if (pathname.startsWith('/cryptocurrencies')) return ['3']
+      if (pathname.startsWith('/articles')) return ['4']
+
+      return []
+    }
+
+    setSelectedKeys(getSelectedKeys())
+  }, [pathname])
 
   return (
-    <Sider collapsible collapsed={collapsed} style={{ height: '100%' }} onCollapse={(value) => setCollapsed(value)}>
+    <Sider
+      theme="dark"
+      collapsible
+      collapsed={collapsed}
+      style={{ height: '100%' }}
+      onCollapse={(value) => setCollapsed(value)}
+    >
       <Link to="/">
         <h1 className={styles.logo}>
           {collapsed ? null : <span>The count of money</span>}
@@ -38,7 +71,7 @@ export const Sidebar = () => {
           </span>
         </h1>
       </Link>
-      <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+      <Menu theme="dark" selectedKeys={selectedKeys} mode="inline" items={menuItems} />
     </Sider>
   )
 }
