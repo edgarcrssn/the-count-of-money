664650450313891f0d37d39cb0712711b4fa82c7
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-12-06 23:34:13 +0100

    feat(ui): :sparkles: created a tour that explain the pages to the user

--- apps/ui/src/app/components/layouts/DashboardLayout/Sidebar/Sidebar.tsx
+++ apps/ui/src/app/components/layouts/DashboardLayout/Sidebar/Sidebar.tsx
@@ -1,8 +1,8 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useRef, useState } from 'react'
 import styles from './Sidebar.module.scss'
 import { AreaChartOutlined, FileTextOutlined, HomeOutlined, UserOutlined } from '@ant-design/icons'
-import type { MenuProps } from 'antd'
-import { Layout, Menu } from 'antd'
+import type { MenuProps, TourProps } from 'antd'
+import { Layout, Menu, Tour } from 'antd'
 import { Link, useLocation } from 'react-router-dom'
 import { CurrentUserContext } from '../../../../context/CurrentUserContext'
 const { Sider } = Layout
@@ -13,10 +13,47 @@ export const Sidebar = () => {
   const { pathname } = useLocation()
 
   const [collapsed, setCollapsed] = useState<boolean>(() => {
-    const storedCollapsed = localStorage.getItem('tcomSidebarCollapsed')
+    const storedCollapsed = localStorage.getItem('sidebarIsCollapsed')
     return storedCollapsed ? JSON.parse(storedCollapsed) : false
   })
   const [selectedKeys, setSelectedKeys] = useState<string[]>([])
+
+  const [isTourOpen, setIsTourOpen] = useState<boolean>(false)
+
+  const handleTourClose = () => {
+    localStorage.setItem('tourHasBeenViewed', 'true')
+    setIsTourOpen(false)
+  }
+
+  const homeLabel = useRef<HTMLAnchorElement>(null)
+  const cryptocurrenciesLabel = useRef<HTMLAnchorElement>(null)
+  const articlesLabel = useRef<HTMLAnchorElement>(null)
+
+  const findParent = (element: HTMLElement, levels: number): HTMLElement => {
+    let currentElement = element
+    for (let i = 0; i < levels; i++) {
+      currentElement = currentElement.parentNode as HTMLElement
+    }
+    return currentElement
+  }
+
+  const steps: TourProps['steps'] = [
+    {
+      title: 'View the dashboard',
+      description: 'Explore combined cryptocurrency and article dashboards for comprehensive insights at a glance.',
+      target: () => findParent(homeLabel.current!, 2),
+    },
+    {
+      title: 'View cryptocurrency prices',
+      description: 'Track real-time cryptocurrency prices on a user-friendly page for informed market insights.',
+      target: () => findParent(cryptocurrenciesLabel.current!, 2),
+    },
+    {
+      title: 'View crypto related articles',
+      description: 'Browse articles on cryptocurrency-related topics for valuable insights and updates.',
+      target: () => findParent(articlesLabel.current!, 2),
+    },
+  ]
 
   const { currentUser } = useContext(CurrentUserContext)
 
@@ -24,7 +61,11 @@ export const Sidebar = () => {
     {
       key: '1',
       icon: <HomeOutlined />,
-      label: <Link to="/">Home</Link>,
+      label: (
+        <Link ref={homeLabel} to="/">
+          Home
+        </Link>
+      ),
     },
     currentUser
       ? {
@@ -36,12 +77,20 @@ export const Sidebar = () => {
     {
       key: '3',
       icon: <AreaChartOutlined />,
-      label: <Link to="/cryptocurrencies">Cryptocurrencies</Link>,
+      label: (
+        <Link ref={cryptocurrenciesLabel} to="/cryptocurrencies">
+          Cryptocurrencies
+        </Link>
+      ),
     },
     {
       key: '4',
       icon: <FileTextOutlined />,
-      label: <Link to="/articles">Articles</Link>,
+      label: (
+        <Link ref={articlesLabel} to="/articles">
+          Articles
+        </Link>
+      ),
     },
   ]
 
@@ -59,26 +108,34 @@ export const Sidebar = () => {
   }, [pathname])
 
   useEffect(() => {
-    localStorage.setItem('tcomSidebarCollapsed', JSON.stringify(collapsed))
+    localStorage.setItem('sidebarIsCollapsed', JSON.stringify(collapsed))
   }, [collapsed])
 
+  useEffect(() => {
+    const hasTourBeenViewed = localStorage.getItem('tourHasBeenViewed')
+    if (!hasTourBeenViewed || hasTourBeenViewed === 'false') setIsTourOpen(true)
+  }, [])
+
   return (
-    <Sider
-      theme="dark"
-      collapsible
-      collapsed={collapsed}
-      style={{ height: '100%' }}
-      onCollapse={(value) => setCollapsed(value)}
-    >
-      <Link to="/">
-        <h1 className={styles.logo}>
-          {collapsed ? null : <span>The count of money</span>}
-          <span role="img" aria-label="Money bag">
-            💰
-          </span>
-        </h1>
-      </Link>
-      <Menu theme="dark" selectedKeys={selectedKeys} mode="inline" items={menuItems} />
-    </Sider>
+    <>
+      <Sider
+        theme="dark"
+        collapsible
+        collapsed={collapsed}
+        style={{ height: '100%' }}
+        onCollapse={(value) => setCollapsed(value)}
+      >
+        <Link to="/">
+          <h1 className={styles.logo}>
+            {collapsed ? null : <span>The count of money</span>}
+            <span role="img" aria-label="Money bag">
+              💰
+            </span>
+          </h1>
+        </Link>
+        <Menu theme="dark" selectedKeys={selectedKeys} mode="inline" items={menuItems} />
+      </Sider>
+      <Tour open={isTourOpen} onClose={handleTourClose} onFinish={handleTourClose} steps={steps} placement="right" />
+    </>
   )
 }
