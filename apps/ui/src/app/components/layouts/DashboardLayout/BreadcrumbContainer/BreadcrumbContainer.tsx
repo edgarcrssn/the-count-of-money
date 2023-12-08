38e97d529978d5d2228863b907f6034bc8509f20
faiza <faizarouibah00@gmail.com>
2023-12-09 00:29:03 +0100

    breadcrumb

--- apps/ui/src/app/components/layouts/DashboardLayout/BreadcrumbContainer/BreadcrumbContainer.tsx
+++ apps/ui/src/app/components/layouts/DashboardLayout/BreadcrumbContainer/BreadcrumbContainer.tsx
@@ -1,33 +1,38 @@
 import React from 'react'
-import { HomeOutlined, UserOutlined } from '@ant-design/icons'
+import { HomeOutlined } from '@ant-design/icons'
 import { Breadcrumb } from 'antd'
-import { useLocation } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
-// TODO https://ant.design/components/breadcrumb
-export const BreadcrumbContainer = () => {
+const BreadcrumbContainer: React.FC = () => {
   const { pathname } = useLocation()
-  console.log(pathname)
+  const pathSnippets = pathname.split('/').filter((i) => i)
 
-  return (
-    <Breadcrumb
-      items={[
-        {
-          href: '',
-          title: <HomeOutlined />,
-        },
-        {
-          href: '',
-          title: (
-            <>
-              <UserOutlined />
-              <span>Application List</span>
-            </>
-          ),
-        },
-        {
-          title: 'Application',
-        },
-      ]}
-    />
-  )
+  const breadcrumbItems = pathSnippets.map((snippet, index) => {
+    const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
+    const isLast = index === pathSnippets.length - 1
+
+    return {
+      link: isLast ? undefined : url,
+      label: isLast ? capitalizeFirstLetter(snippet) : snippet,
+    }
+  })
+
+  breadcrumbItems.unshift({
+    link: '/',
+    label: <HomeOutlined />,
+  })
+
+  return <Breadcrumb>{renderBreadcrumbItems(breadcrumbItems)}</Breadcrumb>
 }
+
+function capitalizeFirstLetter(str: string): string {
+  return str.charAt(0).toUpperCase() + str.slice(1)
+}
+
+function renderBreadcrumbItems(items: { link?: string; label: React.ReactNode }[]) {
+  return items.map((item, index) => (
+    <Breadcrumb.Item key={index}>{item.link ? <Link to={item.link}>{item.label}</Link> : item.label}</Breadcrumb.Item>
+  ))
+}
+
+export default BreadcrumbContainer
