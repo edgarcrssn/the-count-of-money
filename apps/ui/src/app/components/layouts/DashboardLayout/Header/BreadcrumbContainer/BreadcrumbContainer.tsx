742cfed70984961a495908b31eb78b63e13d105b
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-12-09 17:08:02 +0100

    refactor(ui): :recycle: refactored Breadcrumb

---
+++ apps/ui/src/app/components/layouts/DashboardLayout/Header/BreadcrumbContainer/BreadcrumbContainer.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { HomeOutlined } from '@ant-design/icons'
+import { Breadcrumb } from 'antd'
+import { Link, useLocation } from 'react-router-dom'
+import { capitalize } from '../../../../../../utils/capitalize'
+import { BreadcrumbItemType, BreadcrumbSeparatorType } from 'antd/es/breadcrumb/Breadcrumb'
+
+const NON_EXISTING_PATHS = ['profile']
+
+export const BreadcrumbContainer = () => {
+  const { pathname } = useLocation()
+  const pathSnippets = pathname.split('/').filter((i) => i)
+
+  const breadcrumbItems: Partial<BreadcrumbItemType & BreadcrumbSeparatorType>[] = pathSnippets.map(
+    (snippet, index) => {
+      const url = `/${pathSnippets.slice(0, index + 1).join('/')}`
+      const isLast = index === pathSnippets.length - 1
+
+      const label = capitalize(snippet)
+
+      return {
+        title: isLast || NON_EXISTING_PATHS.includes(snippet) ? label : <Link to={url}>{label}</Link>,
+      }
+    },
+  )
+
+  return (
+    <Breadcrumb
+      items={[
+        {
+          title: (
+            <Link to="/">
+              <HomeOutlined />
+            </Link>
+          ),
+        },
+        ...breadcrumbItems,
+      ]}
+    />
+  )
+}
