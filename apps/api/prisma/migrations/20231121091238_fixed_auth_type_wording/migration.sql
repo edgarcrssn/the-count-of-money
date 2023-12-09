376ac953b4829528a0edff458b8dc39fa5d686be
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-12-09 13:58:07 +0100

    refactor(database): :fire: deleted useless migrations since the app is not in production

--- apps/api/prisma/migrations/20231121091238_fixed_auth_type_wording/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `authType` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "authType",
-ADD COLUMN     "auth_type" "AuthType" NOT NULL DEFAULT 'CLASSIC';
--- apps/api/prisma/migrations/20231204113137_add_cryptocurrencies/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Cryptocurrency" (
-    "id" TEXT NOT NULL,
-
-    CONSTRAINT "Cryptocurrency_pkey" PRIMARY KEY ("id")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Cryptocurrency_id_key" ON "Cryptocurrency"("id");
